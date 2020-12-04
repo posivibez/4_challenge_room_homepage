c691d8ca057e39628c0fa5ddc687875cb394b925
Tyler Gumb <tgumb100@gmail.com>
2020-12-04 14:00:39 -0800

    functioning carousel

---
+++ app.js
@@ -0,0 +1,44 @@
+let slidePosition = 0;
+
+const slides = document.getElementsByClassName('carousel__item');
+
+const totalSlides = slides.length;
+
+document.getElementById('carousel__button--next').addEventListener("click", function(){
+    moveToNextSlide();
+});
+
+document.getElementById('carousel__button--previous').addEventListener("click", function(){
+    moveToPreviousSlide();
+});
+
+function updateSlidePosition() {
+    for (let slide of slides) {
+        slide.classList.remove('carousel__item--visible');
+        slide.classList.add('carousel__item--hidden');
+    }
+
+    slides[slidePosition].classList.add('carousel__item--visible');
+}
+
+function moveToNextSlide() {
+    if (slidePosition === totalSlides - 1) {
+        slidePosition = 0;
+    } else {
+        slidePosition++;
+    }
+
+    updateSlidePosition();
+}
+
+function moveToPreviousSlide() {
+    console.log("beginning of func " + slidePosition);
+    if (slidePosition === 0) {
+        slidePosition = totalSlides - 1;
+    } else {
+        slidePosition--;
+    }
+    console.log("end of func " + slidePosition);
+
+    updateSlidePosition();
+}
